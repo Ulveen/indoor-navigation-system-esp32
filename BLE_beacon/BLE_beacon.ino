170d2116e2dfb38bda9e44ba3f0ab5d3bd149545
ulveen <setionoalvin9@gmail.com>
2025-09-27 11:29:59 +0700

    feat: refactor folder structure and add configure tx

--- BLE_beacon/BLE_beacon.ino
+++ BLE_beacon/BLE_beacon.ino
@@ -5,12 +5,13 @@
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  BLEDevice::init("My_ESP32_Beacon");
+  BLEDevice::init("ESP32_Beacon");
   Serial.print("Device MAC Address: ");
   Serial.println(BLEDevice::getAddress().toString().c_str());
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P3);
   pAdvertising->start();
-  Serial.println("BLE advertising started. Look for 'My_ESP32_Beacon' or the MAC address above.");
+  Serial.println("BLE advertising started. Look for 'ESP32_Beacon' or the MAC address above.");
 }
 
 void loop() {}
