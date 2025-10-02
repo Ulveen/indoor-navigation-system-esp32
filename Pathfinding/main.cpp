00f5c81bc4522ba37f096290c16e36af0656d4bf
ulveen <setionoalvin9@gmail.com>
2025-10-02 15:30:10 +0700

    Update: add turning logic

--- Pathfinding/main.cpp
+++ Pathfinding/main.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <windows.h>
+#include <vector>
 
 #define W 15
 #define H 15
 
-#define START_X 13
-#define START_Y 13
-#define END_X 1
-#define END_Y 1
-
-#define EMPTY 0
-#define WALL 1
-#define START 2
-#define END 3
-#define VISITED 4
-#define IN_QUEUE 5
-#define BACKTRACKED 6
+#define WALL '#'
+#define START 'S'
+#define END 'E'
+#define VISITED '.'
+#define IN_QUEUE '*'
 
 #define INVALID -1
-#define FORWARD 0
-#define RIGHT 1
-#define BACKWARD 2
-#define LEFT 3
 
 const int MOVE_X[] = {0, 1, 0, -1};
 const int MOVE_Y[] = {-1, 0, 1, 0};
+const char CURR[] = "^>v<";
+
+int direction = 0;
+
+int startX = 13;
+int startY = 13;
+
+int endX = 1;
+int endY = 1;
 
 using namespace std;
 
@@ -41,6 +39,8 @@ struct Tile
     Tile(int y, int x, int distance, Tile *prev1) : y(y), x(x), distance(distance), prev(prev1) {}
 };
 
+Tile *currTile;
+
 struct TilePriority
 {
     bool operator()(const Tile *a, const Tile *b) const
@@ -49,58 +49,73 @@ struct TilePriority
     }
 };
 
-int world[H][W] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1}};
-int distances[H][W] = {0};
+char map[H][W + 1] = {
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "            #  ",
+    "               ",
+    "               ",
+};
+
+bool visited[H][W] = {false};
+
+int distances[H][W];
 
 int calcDistance(int y1, int x1, int y2, int x2)
 {
     return abs(y1 - y2) + abs(x1 - x2);
 }
 
-int currDirection;
-Tile *currTile;
-
-void initWorld()
+int getDirection(int fromY, int fromX, int targetY, int targetX)
 {
-    world[START_Y][START_X] = START;
-    world[END_Y][END_X] = END;
+    if (targetY < fromY)
+    {
+        return 0;
+    }
+    if (targetX > fromX)
+    {
+        return 1;
+    }
+    if (targetY > fromY)
+    {
+        return 2;
+    }
+    return 3;
 }
 
-void initDistance()
+void init()
 {
+    map[startX][startY] = START;
+    map[endX][endY] = END;
+
     for (int i = 0; i < H; i++)
     {
         for (int j = 0; j < W; j++)
         {
-            distances[i][j] = calcDistance(END_Y, END_X, i, j);
+            distances[i][j] = calcDistance(endY, endX, i, j);
         }
     }
 }
 
-void showWorld()
+void showDistances()
 {
     for (int i = 0; i < H; i++)
     {
         for (int j = 0; j < W; j++)
         {
-            printf("%2d ", world[i][j]);
+            printf("%2d ", distances[i][j]);
         }
-        printf("\n");
+        puts("");
     }
 }
 
@@ -110,9 +125,16 @@ void showMap()
     {
         for (int j = 0; j < W; j++)
         {
-            printf("%2d ", distances[i][j]);
+            if (i == currTile->y && j == currTile->x)
+            {
+                printf("%c", CURR[direction]);
+            }
+            else
+            {
+                printf("%c", map[i][j]);
+            }
         }
-        printf("\n");
+        puts("");
     }
 }
 
@@ -124,83 +146,99 @@ bool isOutOfBounds(int y, int x)
 bool backtrack(Tile *target)
 {
     printf("\n\n");
-    showMap();
+    showDistances();
     printf("Backtracking...\n");
     printf("Target y: %d x: %d\n", target->y, target->x);
     printf("Curr y: %d x: %d\n", currTile->y, currTile->x);
-    scanf("[^\n]");
-    getchar();
+    Sleep(500);
 
     while (currTile)
     {
-        world[currTile->y][currTile->x] = BACKTRACKED;
-        system("cls");
-        printf("\n");
-        showWorld();
-        Sleep(500);
         int distance = calcDistance(target->y, target->x, currTile->y, currTile->x);
         if (distance <= 1)
         {
             return true;
         }
-        currTile = currTile->prev;
+        Tile *prev = currTile->prev;
+        int newDirection = getDirection(currTile->y, currTile->x, prev->y, prev->x);
+
+        if (newDirection != direction)
+        {
+            printf("Turning...\n");
+            direction = newDirection;
+            Sleep(500);
+        }
+
+        currTile = prev;
+
+        system("cls");
+        showMap();
+        Sleep(1000);
     }
     return false;
 }
 
 void solve()
 {
+    priority_queue<Tile *, vector<Tile *>, TilePriority> pq;
+    currTile = new Tile(startY, startX, distances[startY][startX], nullptr);
+    pq.push(currTile);
+    visited[startY][startY] = true;
+
     bool found = false;
 
-    priority_queue<Tile *, vector<Tile *>, TilePriority> pq;
-    currTile = new Tile(START_Y, START_X, distances[START_Y][START_X], nullptr);
-    pq.push(currTile);
-
-    while (!found && !pq.empty())
+    while (!pq.empty() && !found)
     {
         Tile *top = pq.top();
+        pq.pop();
 
-        top->prev = currTile;
-        int distance = calcDistance(top->y, top->x, currTile->y, currTile->x);
-
-        if (distance > 1)
+        if (calcDistance(currTile->y, currTile->x, top->y, top->x) > 1)
         {
             bool success = backtrack(top);
-            if (!success)
+            if (success)
             {
-                printf("Backtrack failed\n");
-                break;
+                printf("Backtrack success\n");
+                Sleep(1000);
             }
             else
             {
-                printf("Backtrack success\n");
+                printf("Backtrack failed\n");
+                Sleep(1000);
+                break;
             }
-            scanf("[^\n]");
-            getchar();
         }
 
-        if (top->y == END_Y && top->x == END_X)
+        int newDirection = getDirection(currTile->y, currTile->x, top->y, top->x);
+
+        if (newDirection != direction)
+        {
+            printf("Turning...\n");
+            direction = newDirection;
+            Sleep(500);
+        }
+
+        map[currTile->y][currTile->x] = VISITED;
+
+        top->prev = currTile;
+        currTile = top;
+
+        if (currTile->y == endY && currTile->x == endX)
         {
             found = true;
             break;
         }
-
-        currTile = top;
-
-        world[top->y][top->x] = VISITED;
-        pq.pop();
 
         for (int i = 0; i < 4; i++)
         {
             int newX = top->x + MOVE_X[i];
             int newY = top->y + MOVE_Y[i];
 
-            if (isOutOfBounds(newY, newX))
+            if (isOutOfBounds(newY, newX) || visited[newY][newX])
             {
                 continue;
             }
 
-            int type = world[newY][newX];
+            char type = map[newY][newX];
 
             if (type == WALL)
             {
@@ -209,55 +247,25 @@ void solve()
 
             int newDistance = distances[newY][newX];
 
-            if (newDistance == INVALID || type == VISITED || type == IN_QUEUE || type == BACKTRACKED)
+            if (newDistance == INVALID)
             {
                 continue;
             }
 
-            world[newY][newX] = IN_QUEUE;
+            map[newY][newX] = IN_QUEUE;
+            visited[newY][newX] = true;
+
             pq.push(new Tile(newY, newX, newDistance, nullptr));
         }
 
         system("cls");
-        showWorld();
+        showMap();
         Sleep(500);
     }
-
-    if (found)
-    {
-        printf("\n\nFOUND!\n\n");
-    }
-    else
-    {
-        printf("\n\nNOT FOUND\n\n");
-    }
-
-    showMap();
-    printf("\n\n");
-    showWorld();
 }
 
 int main()
 {
-    system("cls");
-    printf("Start: %d, End: %d\n", START, END);
-
-    initWorld();
-    printf("World: \n");
-    showWorld();
-
-    printf("\n");
-
-    printf("Map: \n");
-    initDistance();
-    showMap();
-
-    scanf("[^\n]");
-    getchar();
-
-    system("cls");
+    init();
     solve();
-
-    scanf("[^\n]");
-    getchar();
 }
